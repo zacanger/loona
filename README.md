2dba49bea08a271aa898d2b5de7272c1b028d345
zacanger <zac@zacanger.com>
2018-02-20 17:09:01 -0700

    Make it to a thing

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 --------
 
-## Installation
-
 ## Usage
+
+`npx loona`
+
+If you're the sort of person who likes to install things globally,
+`npm i -g loona` and then `loona`
 
 ## License
 
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
 const main = () => {
+  console.log(`
+        stan talent!
+        stan loona!
 
+        (this module coming soon)
+  `)
 }
 
 if (!module.parent) main()
