a35ef74e691a624863ef4ba3fbac1149b513722d
zacanger <zac@zacanger.com>
2018-04-15 12:12:07 -0600

    Add a feature

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 #!/usr/bin/env node
 
-const main = () => {
+const exit = require('zeelib/lib/exit').default
+if (module.parent) {
+  console.log('Please execute this module.')
+  exit(1)
+}
+
+const open = require('zeelib/lib/open').default
+const getArgs = require('zeelib/lib/get-args').default
+const args = getArgs()
+const playlistUrl = 'https://www.youtube.com/watch?v=a6JmCdDs_GM&list=PLtcTpq9htoKzrWNw3PR7hN9IQ0JuxrjQU'
+
+const usage = () => {
   console.log(`
         stan talent!
         stan loona!
 
-        (this module coming soon)
+        usage:
+
+        loona -p [--playlist]
+        # open youtube playlist of all official
+        # MVs with less than 1M views
   `)
 }
 
-if (!module.parent) main()
+if (args.some((a) => a === '-p' || a === '--playlist')) {
+  open(playlistUrl)
+} else {
+  usage()
+}
