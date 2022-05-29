06387b57776bd912fecdcdc14d5f5952a9d2d04e
zacanger <zac@zacanger.com>
2022-05-28 20:03:27 -0600

    feat: make it do something

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
-require('open')('https://www.youtube.com/watch?v=aXaHB4gGAys')
+const open = require('zeelib/lib/open').default
+const prompt = require('zeelib/lib/term-prompt').default
+const isInstalled = require('is-program-installed')
+const spotifyLink = 'https://open.spotify.com/playlist/37i9dQZF1DWSluHnSzEAUI'
+const youtubeLink = 'https://www.youtube.com/watch?v=aXaHB4gGAys'
+
+const main = async () => {
+  const answer = await prompt('Stream Pose on Spotify?')
+  if (answer) {
+    if (isInstalled('spotify')) {
+      open(spotifyLink)
+    } else {
+      console.log('Spotify not installed!')
+      const maybe = await prompt('Open anyway?')
+      if (maybe) {
+        open(spotifyLink)
+      }
+    }
+  }
+
+  const hmm = await prompt('Stream Butterfly on YouTube?')
+  if (hmm) {
+    open(youtubeLink)
+  }
+}
+
+main()
