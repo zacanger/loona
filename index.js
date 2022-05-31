a93d92f954e79af058c10b3211dff5d27f7ce6cf
zacanger <zac@zacanger.com>
2022-05-31 16:39:23 -0600

    feat: add tour message

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ const isInstalled = require('is-program-installed')
 const spotifyLink = 'https://open.spotify.com/playlist/37i9dQZF1DWSluHnSzEAUI'
 const youtubeLink = 'https://www.youtube.com/watch?v=aXaHB4gGAys'
 
+const tourMessage = `LOONA 1ST WORLD TOUR
+
+USA DATES:
+2nd August,  LA,           The Wiltern
+4th August,  SF,           The Midway
+6th August,  Denver,       Fillmore Auditorium
+8th August,  KC,           Uptown Theater
+11th August, Chicago,      Radius Chicago
+13th August, Louisville,   Old Forester's Paristown Hall
+15th August, Reading (PA), Santander Arena
+17th August, DC,           MGM National Harbor
+19th August, NYC,          Palladium Times Sqaure
+
+Tickets available starting June 4th!
+`
+
 const main = async () => {
+  console.log(tourMessage)
   const answer = await prompt('Stream Pose on Spotify?')
   if (answer) {
     if (isInstalled('spotify')) {
