08eaec46c767d27bddb0f0c60ab7590b420847ac
zacanger <zac@zacanger.com>
2022-06-03 17:54:21 -0600

    chore: remove queendom 2 stuff

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 #!/usr/bin/env node
 
-const open = require('zeelib/lib/open').default
-const prompt = require('zeelib/lib/term-prompt').default
-const isInstalled = require('is-program-installed')
-const spotifyLink = 'https://open.spotify.com/playlist/37i9dQZF1DWSluHnSzEAUI'
-const youtubeLink = 'https://www.youtube.com/watch?v=aXaHB4gGAys'
-const mnetLink = 'https://www.mnet.world/community/main?communityId=DtAok2aQcE21eCPykh19t'
-const guideLink = 'https://old.reddit.com/r/LOONA/comments/v15kai/queendom_finale_full_guide_discussion_thread/'
-
-const tourMessage = `LOONA 1ST WORLD TOUR
+const tourMessage = `
+LOONA 1ST WORLD TOUR
 
 USA DATES:
 2nd August,  LA,           The Wiltern
@@ -22,37 +15,10 @@ USA DATES:
 19th August, NYC,          Palladium Times Sqaure
 
 Tickets available starting June 4th!
-`
+`.trim()
 
 const main = async () => {
   console.log(tourMessage)
-  const guide = await prompt('See voting guide for Queendom?')
-  if (guide) {
-    open(guideLink)
-  }
-
-  const first = await prompt('Vote for LOONA on MNET?')
-  if (first) {
-    open(mnetLink)
-  }
-
-  const answer = await prompt('Stream Pose on Spotify?')
-  if (answer) {
-    if (isInstalled('spotify')) {
-      open(spotifyLink)
-    } else {
-      console.log('Spotify not installed!')
-      const maybe = await prompt('Open anyway?')
-      if (maybe) {
-        open(spotifyLink)
-      }
-    }
-  }
-
-  const hmm = await prompt('Stream Butterfly on YouTube?')
-  if (hmm) {
-    open(youtubeLink)
-  }
 }
 
 main()
