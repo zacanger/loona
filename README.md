72c0b17290f47790b311aa318e17ca67a0130f85
zacanger <zac@zacanger.com>
2022-08-13 10:00:18 -0600

    chore: update

--- README.md
+++ README.md
@@ -12,6 +12,7 @@
 * [Loona Content Archive](https://docs.google.com/spreadsheets/d/1XbLfgf4grfsZ_CUSt9nXgHVFoMrgu16wgVPYNs1hAX4/edit#gid=1204245601)
 * [Team Subbits](https://www.teamsubbits.com)
 * [Loona Photo Archive](https://drive.google.com/drive/folders/1k0dVWFyxa6VUdwno97vsJZK3MttvxwQj)
+* [Image Dump Masterlist](https://docs.google.com/spreadsheets/d/128qKdqfKLLSWN8YBQTDRsTNPK1WEugN5cAz1hyRVTj8/edit#gid=1979688804)
 
 ## License
 
--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const contentArchiveLink = 'https://docs.google.com/spreadsheets/d/1XbLfgf4grfsZ
 const photoArchiveLink = 'https://drive.google.com/drive/folders/1k0dVWFyxa6VUdwno97vsJZK3MttvxwQj'
 const teamSubbitsLink = 'https://www.teamsubbits.com'
 const fandomLink = 'https://loonatheworld.fandom.com/'
+const imageDumpLink = 'https://docs.google.com/spreadsheets/d/128qKdqfKLLSWN8YBQTDRsTNPK1WEugN5cAz1hyRVTj8/edit#gid=1979688804'
 
 const main = async () => {
   console.log(colors.green(tourMessage))
@@ -41,6 +42,9 @@ const main = async () => {
   console.log()
   console.log(colors.magenta('Photo Archive:'))
   console.log(colors.magenta(photoArchiveLink))
+  console.log()
+  console.log(colors.blue('Image Dump Masterlist:'))
+  console.log(colors.blue(imageDumpLink))
   console.log()
   console.log(colors.red('Team Subbits:'))
   console.log(colors.red(teamSubbitsLink))
