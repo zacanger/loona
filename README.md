066e049bf539e6d0f08429d0c11e94c21975ef97
zacanger <zac@zacanger.com>
2022-08-13 09:57:31 -0600

    chore: more updates

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
 * [Loona Fandom Wiki](https://loonatheworld.fandom.com/)
 * [Loona Content Archive](https://docs.google.com/spreadsheets/d/1XbLfgf4grfsZ_CUSt9nXgHVFoMrgu16wgVPYNs1hAX4/edit#gid=1204245601)
 * [Team Subbits](https://www.teamsubbits.com)
+* [Loona Photo Archive](https://drive.google.com/drive/folders/1k0dVWFyxa6VUdwno97vsJZK3MttvxwQj)
 
 ## License
 
--- index.js
+++ index.js
@@ -30,16 +30,24 @@ Japan:
 
 const contentArchiveLink = 'https://docs.google.com/spreadsheets/d/1XbLfgf4grfsZ_CUSt9nXgHVFoMrgu16wgVPYNs1hAX4/edit#gid=1204245601'
 const photoArchiveLink = 'https://drive.google.com/drive/folders/1k0dVWFyxa6VUdwno97vsJZK3MttvxwQj'
+const teamSubbitsLink = 'https://www.teamsubbits.com'
+const fandomLink = 'https://loonatheworld.fandom.com/'
 
 const main = async () => {
   console.log(colors.green(tourMessage))
-  console.log('\n')
+  console.log()
   console.log(colors.cyan('Content Archive:'))
   console.log(colors.cyan(contentArchiveLink))
-  console.log('\n')
+  console.log()
   console.log(colors.magenta('Photo Archive:'))
   console.log(colors.magenta(photoArchiveLink))
-  console.log('\n')
+  console.log()
+  console.log(colors.red('Team Subbits:'))
+  console.log(colors.red(teamSubbitsLink))
+  console.log()
+  console.log(colors.yellow('Fandom Wiki:'))
+  console.log(colors.yellow(fandomLink))
+  console.log()
   const answer = await prompt('Open Flip That on YouTube?')
   if (answer) {
     open('https://www.youtube.com/watch?v=SPYX2y4NzTU')
