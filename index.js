48c05c71e1683b8842af4fcc3055f6c66f845f55
zacanger <zac@zacanger.com>
2022-11-13 18:31:03 -0700

    chore: rm package

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
 
-const colors = require('zeelib/lib/colorize').default
+const { inspect } = require('util')
+
+const cols = (color, text) => {
+  const codes = inspect.colors[color]
+  return `\x1b[${codes[0]}m${text}\x1b[${codes[1]}m`
+}
+
+const colorize = () => {
+  const val = {}
+  Object.keys(inspect.colors).forEach((color) => {
+    val[color] = (text) => cols(color, text)
+  })
+  return val
+}
+
+const colors = colorize()
 
 const contentArchiveLink = 'https://docs.google.com/spreadsheets/d/1XbLfgf4grfsZ_CUSt9nXgHVFoMrgu16wgVPYNs1hAX4/edit#gid=1204245601'
 const photoArchiveLink = 'https://drive.google.com/drive/folders/1k0dVWFyxa6VUdwno97vsJZK3MttvxwQj'
